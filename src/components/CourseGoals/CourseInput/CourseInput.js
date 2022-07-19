d4a292a6ecd38663a6fb95750f58596daccb3827
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-07-19 19:22:26 +0530

    clear the imput field after submitting the form

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -29,15 +29,13 @@ import styles from "./CourseInput.module.css";
 //   border-color: #8b005d;
 // }
 
-
-
 // `;
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
-    if(event.target.value.trim().length >0){
+    if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
@@ -50,17 +48,17 @@ const CourseInput = (props) => {
       return;
     }
     props.onAddGoal(enteredValue);
+    setEnteredValue("");
+
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
-
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
-        <input
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
+        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
